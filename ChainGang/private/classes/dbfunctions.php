db29ec2f0a6ea9ef835add267c3ac23ad1ba06a8
Perensoep109 <wiebevaken@gmail.com>
2019-05-08 12:36:35 +0200

    Added func logError

--- ChainGang/private/classes/dbfunctions.php
+++ ChainGang/private/classes/dbfunctions.php
@@ -14,22 +14,40 @@ class DBI
         $returnVal = new mysqli("", "", "", "");
 
         if($returnVal->connect_error)
-            die("Connection failed, " . $returnVal->connect_error);
+            self::logError("Connection failed, " . $returnVal->connect_error);
 
         return $returnVal;
     }
 
-    // This function queries the database, then assembles the DB data into an array of bikes
-    public static function queryBikes($query)
+    /*
+     * This function logs any error which is given as an HTML element anywhere on the page.
+     * String-$error stands for the error which must be logged
+     * This function gets executed from query functions
+     */
+    private static function logError($error)
+    {
+        print($error);
+    }
+
+    /*  This function queries the database, then assembles the DB data into an array of bikes
+        String-$query stands for the query which will be executed on the database
+        Bool-$logerror stands for if the error must be logged as HTML
+    */
+    public static function queryBikes($query, $logerror)
     {
         $dbConn = self::makeDBConn();
 
         if(is_null($dbConn))
-            return;
+        {
+            if($logerror)
+                self::logError("ERROR: Querying data from database created an error: $dbConn->error");
+        }
+
+
     }
 
     // This function queries the database, then assembles the DB data into an array of users
-    public static function queryUsers($query)
+    public static function queryUsers($query, $logerror)
     {
     }
 }
