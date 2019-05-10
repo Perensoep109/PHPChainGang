e0c1c7e82df0ce7992fa02f7ea2f4e2cdcc619d3
eugene <eugene.take@student.graafschapcollege.nl>
2019-05-10 15:30:47 +0200

    Header opnieuw gebouwd
    header opnieuw gemaakt met bootstrap

--- ChainGang/static/header.php
+++ ChainGang/static/header.php
@@ -1,17 +1,53 @@
-<div class="row">
-    <div class="col-sm-8">
-      One of three columns
+<div class="row" id="nav_header" >
+    <div class="col-md-6"></div>
+    <div class="col-md-1 text-center">
+    <button type="button" class="btn btn-link">Help</button>
     </div>
-    <div class="col-sm-1">
-      Button
+    <div class="col-md-2 text-center">
+    <button type="button" class="btn btn-link">Over ons</button>
     </div>
-    <div class="col-sm-1">
-      Button
+    <div class="col-md-1 text-center">
+    <button type="button" class="btn btn-link">Contact</button>
     </div>
-    <div class="col-sm-1">
-      Button
-    </div>
-    <div class="col-sm-1">
-      Button
+    <div class="col-md-2 text-center">
+    <button type="button" class="btn btn-link">Login/Registeren</button>
     </div>
   </div>
+  <div class="row">
+      <div class="col-md-2">
+        <h1 class="nav_logo">FietsShop</h1>
+      </div>
+      <div class="col-md-10 col-auto" id="nav_SearchAndButton">
+          <div class="input-group" >
+              <div class="input-group-prepend">
+                  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</button>
+                  <div class="dropdown-menu">
+<!--                      Deze zullen moeten gegenereerd worden van de databases-->
+                      <a class="dropdown-item" href="#">Mannen</a>
+                      <a class="dropdown-item" href="#">Vrouwen</a>
+                      <a class="dropdown-item" href="#">Kinderen</a>
+                  </div>
+              </div>
+              <input type="text" class="form-control" aria-label="Text input with dropdown button">
+          </div>
+
+      </div>
+  </div>
+<div class="row">
+
+    <div class="col-md-2">
+        <button type="button text-center" class="btn btn-link">Thuis</button>
+    </div>
+    <div class="col-md-2 text-center">
+        <button type="button" class="btn btn-link">Fietsen</button>
+    </div>
+    <div class="col-md-2 text-center">
+        <button type="button" class="btn btn-link">Mijn profiel</button>
+    </div>
+    <div class="col-md-2 text-center">
+        <button type="button" class="btn btn-link">Over ons</button>
+
+    </div>
+    <div class="col-md-3"></div>
+    <div class="col-md-1" style="background-color: red"></div>
+</div>
