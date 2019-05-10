938ca7015a78b09052c1976470ceffc229f6ef09
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2019-05-10 10:25:43 +0200

    Nodige changes om laragon werkende te maken

--- ChainGang/public/index.php
+++ ChainGang/public/index.php
@@ -8,7 +8,7 @@
     <link href="../plugins/bootstrap-4.3.1/css/bootstrap.min.css" rel="stylesheet" />
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>C:\Users\Werner\Desktop\repos\PHPChainGang\ChainGang\
     <![endif]-->
 </head>
 <body>
