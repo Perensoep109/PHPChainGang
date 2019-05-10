0dce9fecb1406e2548db43f7d9f7f0d2396928dc
eugene <eugene.take@student.graafschapcollege.nl>
2019-05-10 14:23:16 +0200

    header verwijderd
    ga het opbouwen vanuit Bootstrap

--- ChainGang/static/header.php
+++ ChainGang/static/header.php
@@ -1,25 +1,17 @@
-<div id="header">
-    <nav>
-        <div id="navtop">
-            <ul>
-                <li class="navitem"><a>Help</a></li>
-                <li class="navitem"><a>Over ons</a></li>
-                <li class="navitem"><a>Contact</a></li>
-                <li class="navitem"><a>Login</a></li>
-            </ul>
-        </div>
-        <div id="LogoAndSearch">
-        <div id="icon"> FietsShop</div>
-        <div id="search">
-            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Catogorie<span
-                        class="caret"></span></button>
-            <ul class="dropdown-menu">
-                <li><a href="#">Mannen</a></li>
-                <li><a href="#">Vrouwen</a></li>
-                <li><a href="#">Kinderen</a></li>
-            </ul>
-            <input type="text" class="form-control" id="searchBox">
-        </div>
-</div>
-</nav>
-</div>
+<div class="row">
+    <div class="col-sm-8">
+      One of three columns
+    </div>
+    <div class="col-sm-1">
+      Button
+    </div>
+    <div class="col-sm-1">
+      Button
+    </div>
+    <div class="col-sm-1">
+      Button
+    </div>
+    <div class="col-sm-1">
+      Button
+    </div>
+  </div>
