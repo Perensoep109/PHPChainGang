8d5189e7012d9d52f84dc4afabd30b979d7e752e
eugene <eugene.take@student.graafschapcollege.nl>
2019-05-12 22:19:00 +0200

    incluce_once aangepast
    homepage include_once(path) aangepast

--- ChainGang/public/webpages/HomePage.php
+++ ChainGang/public/webpages/HomePage.php
@@ -15,15 +15,11 @@
 </head>
 <body>
 <div class="container">
-<?php
-include_once("../../static/header.php")
-?>
+        <?php include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/static/header.php")?>
 
 <h1>code for home page here</h1>
 
-<?php
-include_once("../../static/footer.php")
-?>
+    <?php include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/static/footer.php")?>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
