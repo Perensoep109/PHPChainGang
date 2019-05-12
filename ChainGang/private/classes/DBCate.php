93cdc4030cb9e5ee5ad4bd49ad345e5104459f5b
eugene <eugene.take@student.graafschapcollege.nl>
2019-05-12 19:34:29 +0200

    Update DBCate.php
    de klasse DBCate ingevuld

--- ChainGang/private/classes/DBCate.php
+++ ChainGang/private/classes/DBCate.php
@@ -3,4 +3,25 @@
 class DBCate
 {
 
+
+     //==Variables==//
+     private $dbIndex;
+     private $name;
+
+
+     //==GettersandSetters==//
+     //==Getters==//
+     public function getDbIndex()
+     {
+         return $this->dbIndex;
+     }
+     public function getName()
+     {
+         return $this->name;
+     }
+     //==Setters==//
+     //you cant change an catogory in the db so no setters here
+
+
+
 }
