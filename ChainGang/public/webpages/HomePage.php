e0514fb785e0a21c0a09d59f04ffd9a6e8131f32
eugene <eugene.take@student.graafschapcollege.nl>
2019-05-12 20:32:14 +0200

    kleine aanpassingen
    aanpassing voor de include once paths in de home page
    header meer leesbaar gemaakt en kleine aanpassingen
    stylesheet aangepast zodat de kleuren goed zijn in de header

--- ChainGang/public/webpages/HomePage.php
+++ ChainGang/public/webpages/HomePage.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>FietsShop</title>
+    <link rel="stylesheet" type="text/css" href="../stylesheets/headerStyle.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!--[if lt IE 9]>
@@ -15,13 +16,13 @@
 <body>
 <div class="container">
 <?php
-include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/static/header.php")
+include_once("../../static/header.php")
 ?>
 
 <h1>code for home page here</h1>
 
 <?php
-include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/static/footer.php")
+include_once("../../static/footer.php")
 ?>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- ChainGang/static/header.php
+++ ChainGang/static/header.php
@@ -12,7 +12,8 @@
     </div>
     <div class="col-md-2 text-center">
         <button type="button" class="btn btn-link" data-toggle="modal" data-target=".login-modal">Login/Registeren</button>
-            <!--hier moet de login form komen-->
+            <!--^^hier is de button voor de Login form-->
+
     </div>
 </div>
 
@@ -24,8 +25,8 @@
     <div class="col-md-10 col-auto" id="header_SearchAndButton">
         <div class="input-group">
             <div class="input-group-prepend">
-                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">Catogorieën
+                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Catogorieën
                 </button>
                 <div class="dropdown-menu">
                     <!--                      Deze zullen moeten gegenereerd worden van de databases-->
@@ -34,7 +35,10 @@
                     <a class="dropdown-item" href="#">Kinderen</a>
                 </div>
             </div>
-            <input type="text" class="form-control" aria-label="Text input with dropdown button">
+            <input type="text" class="form-control" id="SearchInput" aria-label="Text input with dropdown button">
+            <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="material-icons">search</i></button>
+            </div>
         </div>
 
     </div>
@@ -69,6 +73,7 @@
                 </div>
             </button>
             <div class="dropdown-menu">
+                <!-- hier is de shopping cart-->
                 <h1>shopping cart goes here i guess</h1>
             </div>
         </div>
@@ -80,13 +85,8 @@
             <div class="modal-content">
                 <h1>LOGIN FORM HERE</h1>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac magna non urna fermentum cursus. Maecenas egestas egestas ipsum, eget consectetur eros imperdiet id. Nulla aliquam pulvinar ante, et auctor risus fermentum quis. Donec sit amet leo vel dolor consectetur finibus. Etiam ut faucibus nunc, ac semper metus. Phasellus varius vehicula suscipit. Cras quis neque in nisi facilisis suscipit.
-
-                Sed porttitor sem finibus nibh ullamcorper tempor. Aenean lobortis risus eget leo viverra eleifend. Ut placerat egestas mi ut suscipit. Nam quam erat, dictum non maximus id, facilisis vitae est. Mauris ultrices tempor dolor quis lacinia. Aliquam erat volutpat. Mauris nisl ante, suscipit eget rutrum nec, auctor ac neque. Proin semper felis at mi interdum pharetra. Mauris tincidunt magna ac molestie vulputate.
-
-                Nullam at dolor rhoncus, rhoncus urna quis, imperdiet urna. Suspendisse sit amet tellus felis. Vivamus ante lectus, porttitor et felis at, elementum efficitur odio. Aliquam sed nisl non urna pretium rhoncus nec sed justo. Praesent eget nulla at diam ultricies ultrices eget nec sem. Maecenas eu elit nec odio mattis feugiat. Mauris at mollis magna. Praesent aliquet turpis vehicula nunc eleifend, eget accumsan odio tempor. Sed et mollis nunc. Vestibulum cursus lorem vel mauris tempor tempus. Quisque eu commodo nulla, eu auctor nisi. Vestibulum justo ante, efficitur consectetur lorem sed, iaculis feugiat ex. Phasellus posuere congue porttitor. Nulla bibendum convallis vulputate. Aliquam cursus, orci nec eleifend volutpat, enim diam placerat quam, vitae blandit mi enim non leo. Proin facilisis quam at sapien tincidunt dapibus.
-
+                Sed porttitor sem finibus nibh ullamcorper tempor. Aenean lobortis risus eget leo viverra eleifend. Ut placerat egestas mi ut suscipit. Nam quam erat, dictum non maximus id, facilisis vitae est. Mauris ultrices tempor dolor quis lacinia. Aliquam erat volutpat. Mauris nisl ante, suscipit eget rutrum nec, auctor ac neque. Proin semper felis at mi interdum pharetra. Mauris tincidunt magna ac molestie vulputate. Nullam at dolor rhoncus, rhoncus urna quis, imperdiet urna. Suspendisse sit amet tellus felis. Vivamus ante lectus, porttitor et felis at, elementum efficitur odio. Aliquam sed nisl non urna pretium rhoncus nec sed justo. Praesent eget nulla at diam ultricies ultrices eget nec sem. Maecenas eu elit nec odio mattis feugiat. Mauris at mollis magna. Praesent aliquet turpis vehicula nunc eleifend, eget accumsan odio tempor. Sed et mollis nunc. Vestibulum cursus lorem vel mauris tempor tempus. Quisque eu commodo nulla, eu auctor nisi. Vestibulum justo ante, efficitur consectetur lorem sed, iaculis feugiat ex. Phasellus posuere congue porttitor. Nulla bibendum convallis vulputate. Aliquam cursus, orci nec eleifend volutpat, enim diam placerat quam, vitae blandit mi enim non leo. Proin facilisis quam at sapien tincidunt dapibus.
                 Phasellus pretium pellentesque sollicitudin. Donec lacus orci, placerat nec facilisis sed, rhoncus eu ante. Etiam et odio lectus. Ut a lacus dapibus, porttitor nibh eget, mollis purus. Cras vitae magna fringilla, viverra quam non, tempor arcu. In hac habitasse platea dictumst. Etiam vulputate ac metus quis egestas. Fusce cursus elementum leo id fermentum. Duis ac sollicitudin risus.
-
                 Suspendisse quis nisl vel ex malesuada tempor. Phasellus porta sapien a consequat pellentesque. Integer at lorem elit. Vivamus sed tellus fringilla, bibendum turpis eget, tincidunt mi. Nullam et magna vestibulum, consectetur lacus sed, suscipit ante. Etiam non nunc ac mauris tincidunt condimentum. Aenean ac tortor porta, venenatis ex quis, blandit libero. Vestibulum nec viverra quam. Fusce quis tortor urna. Duis mi turpis, aliquam sit amet fermentum sit amet, semper venenatis magna. Duis volutpat eget augue at gravida.
             </div>
         </div>
