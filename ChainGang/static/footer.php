3603619984e3e571cc960fdb95e644321a0757f4
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2019-05-13 13:45:36 +0200

    Footer geinclude in detailpagina
    Tevens social buttons toegevoegd, werkt nog niet naar behoren (JQuery mist)

--- ChainGang/static/footer.php
+++ ChainGang/static/footer.php
@@ -1,20 +1,35 @@
 <div id="footer">
     <hr>
-    <div id="footer_links">
-        <ul class="list-unstyled">
-            <li>Categoriën</li>
-            <li><a href="*">Mannenfietsen</a></li>
-            <li><a href="*">Vrouwenfietsen</a></li>
-            <li><a href="*">Kinderfietsen</a></li>
-        </ul>
-        <ul class="list-unstyled">
-            <li>Other</li>
-            <li><a href="*">Help</a></li>
-            <li><a href="*">Over ons</a></li>
-            <li><a href="*">Contact</a></li>
-        </ul>
+    <div id="row">
+        <!--links for categorien en other-->
+        <div class="col-lg-1">
+            <ul class="list-unstyled">
+                <li>Categoriën</li>
+                <li><a href="*">Mannenfietsen</a></li>
+                <li><a href="*">Vrouwenfietsen</a></li>
+                <li><a href="*">Kinderfietsen</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-4">
+            <ul class="list-unstyled">
+                <li>Other</li>
+                <li><a href="*">Help</a></li>
+                <li><a href="*">Over ons</a></li>
+                <li><a href="*">Contact</a></li>
+            </ul>
+        </div>
+        <div>
+            <!--social media links-->
+            <p>Volg ons op social media!</p>
+
+            <!-- fancypancy icoontjes -->
+            <a class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fab fa-twitter"></i></a>
+            <a class="btn-floating btn-lg btn-gplus" type="button" role="button"><i
+                        class="fab fa-google-plus-g"></i></a>
+        </div>
     </div>
-    <p>Volg ons op social media!</p>
+
     <hr>
-    <p>&#9400; Fietsshop Nederland 2019</p>
+    <center>&#9400; Fietsshop Nederland 2019</center>
 </div>
