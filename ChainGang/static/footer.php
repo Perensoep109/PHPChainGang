4c309d6911398a27a7e690e028702ca8a31497b0
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2019-05-13 10:03:55 +0200

    Header toegevoegd
    in progress

--- ChainGang/static/footer.php
+++ ChainGang/static/footer.php
@@ -1,3 +1,20 @@
 <div id="footer">
-
+    <hr>
+    <div id="footer_links">
+        <ul class="list-unstyled">
+            <li>Categoriën</li>
+            <li><a href="*">Mannenfietsen</a></li>
+            <li><a href="*">Vrouwenfietsen</a></li>
+            <li><a href="*">Kinderfietsen</a></li>
+        </ul>
+        <ul class="list-unstyled">
+            <li>Other</li>
+            <li><a href="*">Help</a></li>
+            <li><a href="*">Over ons</a></li>
+            <li><a href="*">Contact</a></li>
+        </ul>
+    </div>
+    <p>Volg ons op social media!</p>
+    <hr>
+    <p>&#9400; Fietsshop Nederland 2019</p>
 </div>
