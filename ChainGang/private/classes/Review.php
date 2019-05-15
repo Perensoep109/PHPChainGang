4029edd1e9fa2f1ac4f7ae79a85cf1f4a108dc01
eugene <eugene.take@student.graafschapcollege.nl>
2019-05-15 11:57:30 +0200

    bezig met reviews

---
+++ ChainGang/private/classes/Review.php
@@ -0,0 +1,28 @@
+<?php
+
+class Review
+{
+    public function __construct($dbReview)
+    {
+       echo "<ul class='list-unstyled'>";
+        echo "<li class='media'>
+<img src='/chaingang/public/images/UserTEMP.png' class='mr-3 img-thumbnail user-icon'>
+    <div class='media-body'>
+      <h5 class='mt-0 mb-1'>List-based media object</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>";
+    }
+}/*
+ * class review
+ *      $dbReview;
+ *
+ *      constructor(review)
+ *          $dbReview = review;
+ *
+ *
+ * for()
+ * {
+ *  new review($dbreview);
+ * }
+ */
--- ChainGang/private/functions/dbfunctions.php
+++ ChainGang/private/functions/dbfunctions.php
@@ -11,6 +11,7 @@ include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/private/classes/DBUser.php");
 include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/private/classes/DBReview.php");
 include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/private/classes/DBOrder.php");
 include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/private/classes/DBCate.php");
+include_once("$_SERVER[DOCUMENT_ROOT]/chaingang/private/classes/Review.php");
 //include_once("../classes/DBOrder.php");
 //include_once("../classes/DBReview.php");
 //include_once("../classes/DBUser.php");
