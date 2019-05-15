00a61729965e4d2f7d900e9cdbbe1c30464a710c
eugene <eugene.take@student.graafschapcollege.nl>
2019-05-15 15:48:26 +0200

    Update Review.php
    code er iets beter uit laten zien

--- ChainGang/private/classes/Review.php
+++ ChainGang/private/classes/Review.php
@@ -7,29 +7,27 @@ class Review
         echo "<ul class='list-unstyled'>";
         foreach ($dbReview as $item) {
             $user = DBI::queryUsers('select * from allusers WHERE USER_ID = ' . $item->getUserID());
-            echo "<li class='media'>
-<img src='/chaingang/public/images/UserTEMP.png' class='mr-3 img-thumbnail user-icon'>
-    <div class='media-body'>
-    <div class='row'>
-    <div class='col-lg-2'>
-    <h5>" . $user[0]->getName() . "</h5>
-</div>
-<div class='col-lg-6'></div>
-    <div class='col-lg-2'>
-    <h5>" . date("Y/m/d",strtotime($item->getTimePlaced())) . "</h5>
-</div>
-      <div class='row'>
-      <div class='col-lg-12'>
-            <div class='bubble'>" . $item->getText() . "</div>
-</div>
-
-</div>
-      
-</div>
-      
-      
+            echo "
+<li class='media'>
+    <img src='/chaingang/public/images/UserTEMP.png' class='mr-3 img-thumbnail user-icon'>
+        <div class='media-body'>
+            <div class='row'>
+                <div class='col-lg-2'>
+                    <h5>" . $user[0]->getName() . "</h5>
+                </div>
+                <div class='col-lg-6'></div>
+                <div class='col-lg-2'>
+                    <h5>" . date("Y/m/d",strtotime($item->getTimePlaced())) . "</h5>
+                </div>
+                <div class='row'>
+                <div class='col-lg-12'>
+                <div class='bubble'>" . $item->getText() . "
+                </div>
+             </div>
+        </div>
     </div>
-  </li>
+</div>
+</li>
   <hr>";
         }
 
