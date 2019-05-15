e4f81003d7e5ebde7bca8f0666f4fac0c666c672
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2019-05-15 11:52:31 +0200

    paar aanpassingen en toevoegingen
    Header en footer geinclude bij CategoriePage, aanpassingen BikeProduct

--- ChainGang/private/classes/BikeProduct.php
+++ ChainGang/private/classes/BikeProduct.php
@@ -3,13 +3,16 @@
 
 class BikeProduct
 {
-
+    /*
+     * aanmaken van een bootstrap cart met data uit DB
+     */
     public function _construct()
     {
         echo "<div>
 
 </div>";
     }
+
 
 
 }
