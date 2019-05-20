8073b0bb647ffa411eed7651e8c73febc8597208
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2019-05-20 13:03:59 +0200

    Back on it
    Alles hersteld

--- ChainGang/private/classes/BikeProduct.php
+++ ChainGang/private/classes/BikeProduct.php
@@ -1,18 +1,33 @@
 <?php
 
+include_once "$_SERVER[DOCUMENT_ROOT]/ChainGang/private/classes/DBBike.php";
 
 class BikeProduct
 {
+
     /*
      * aanmaken van een bootstrap cart met data uit DB
      */
-    public function _construct()
+    public function __construct(DBBike $fiets)
     {
-        echo "<div>
+        //?ID=3
+        ?>
+        <div class="card-deck">
+            <div class="card">
+                <img class="card-img-top" src="<?php echo $fiets->getImagePaths() ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $fiets->getName() ?></h5>
+                    <p class="card-text"><?php echo $fiets->getCategory() ?></p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
 
-</div>";
+        <?php
     }
 
-
-
 }
+
+?>
