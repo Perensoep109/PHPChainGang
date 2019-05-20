e9a2b43bea9508ff67ec700bfc355c4dbc674a02
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2019-05-20 19:19:30 +0200

    Link van card maken
    probeer een link van de card te maken. Nog niet helemaal een succes

--- ChainGang/private/classes/BikeProduct.php
+++ ChainGang/private/classes/BikeProduct.php
@@ -11,16 +11,18 @@ class BikeProduct
     public function __construct(DBBike $fiets)
     {
         ?>
-        <div class="card cardbike">
-            <img class="card-img-top" src="<?php echo "../" . $fiets->getImagePaths()[0] ?>" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $fiets->getName() ?></h5>
-                <p class="card-text cardbike_price">&euro;<?php echo $fiets->getPrice() ?></p>
-                <p class="card-text">
-                    <b>Omschrijving: </b><?php echo $string = substr($fiets->getDescription(), 0, 75); ?><a
-                            href="DetailPage.php"> ...read more</a></p>
+        <a href="DetailPage.php" class="cardbike_link">
+            <div class="card cardbike">
+                <img class="card-img-top" src="<?php echo "../" . $fiets->getImagePaths()[0] ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $fiets->getName() ?></h5>
+                    <p class="card-text cardbike_price">&euro;<?php echo $fiets->getPrice() ?></p>
+                    <p class="card-text">
+                        <b>Omschrijving: </b><?php echo $string = substr($fiets->getDescription(), 0, 75); ?><a
+                                href="DetailPage.php"> ...read more</a></p>
+                </div>
             </div>
-        </div>
+        </a>
 
         <?php
     }
