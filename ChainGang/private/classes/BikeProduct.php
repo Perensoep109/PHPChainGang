3cf7b1aebf0dad70b9cfdd4f447ec00d1b23f5bf
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2019-05-20 13:45:57 +0200

    Card aanpassingen
    Lengte van de discription aangepast

--- ChainGang/private/classes/BikeProduct.php
+++ ChainGang/private/classes/BikeProduct.php
@@ -10,18 +10,18 @@ class BikeProduct
      */
     public function __construct(DBBike $fiets)
     {
-        //?ID=3
         ?>
-            <div class="card cardbike">
-                <img class="card-img-top" src="<?php echo "../" . $fiets->getImagePaths()[0] ?>" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo $fiets->getName() ?></h5>
-                    <p class="card-text"><?php echo $fiets->getCategory() ?></p>
-                    <p class="card-text">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </p>
-                </div>
+        <div class="card cardbike">
+            <img class="card-img-top" src="<?php echo "../" . $fiets->getImagePaths()[0] ?>" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $fiets->getName() ?></h5>
+                <p class="card-text"><?php echo $string = substr($fiets->getDescription(), 0, 140); ?><a
+                            href="DetailPage.php"> ...read more</a></p>
+                <p class="card-text">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </p>
             </div>
+        </div>
 
         <?php
     }
